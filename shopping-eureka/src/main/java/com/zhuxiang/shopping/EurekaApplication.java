9b48cb7c2900970612bd06e38c4bb6812fb11023
zhuxiang <zx@facemeng.com.cn>
2018-09-03 16:38:07 +0800

    添加服务注册-eureka

---
+++ shopping-eureka/src/main/java/com/zhuxiang/shopping/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.zhuxiang.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: zhuxiang
+ * @date: 2018/9/3 16:23
+ * @description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
